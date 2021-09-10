da500b4429f9bcd5ae50c9c67c2b925c692f84fc
Sang-MHL <89411234+Sang-MHL@users.noreply.github.com>
2021-09-09 17:51:16 -0700

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -2405,7 +2405,11 @@ class FTPSyncProvider {
             this.logger.all(`----------------------------------------------------------------`);
             this.logger.all(`🎉 Sync complete. Saving current server state to "${this.serverPath + this.stateName}" ___ "${this.localPath}"`);
             if (this.dryRun === false) {
-                yield utilities_1.retryRequest(this.logger, () => __awaiter(this, void 0, void 0, function* () { return yield this.client.uploadFrom(this.serverPath + this.stateName, this.stateName); }));
+		    if(this.serverPath){
+			yield utilities_1.retryRequest(this.logger, () => __awaiter(this, void 0, void 0, function* () { return yield this.client.uploadFrom('.'+this.serverPath + this.stateName, this.stateName); }));
+		    }else{
+                	yield utilities_1.retryRequest(this.logger, () => __awaiter(this, void 0, void 0, function* () { return yield this.client.uploadFrom(this.localPath + this.stateName, this.stateName); }));
+		    }
             }
         });
     }
